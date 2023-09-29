aa426c18560bf2a70c1f8eb0d7f9d29d0f2ecfe6
Xabier Monsalve <xabi@xabi.local>
2023-09-29 09:24:04 -0300

    trate de hacer los metodos crud y para buscar

--- app/Http/Controllers/Tareas.php
+++ app/Http/Controllers/Tareas.php
@@ -6,5 +6,85 @@ use Illuminate\Http\Request;
 
 class Tareas extends Controller
 {
-    //
+
+public function index()
+{
+    $tareas = Tarea::all();
+    return view('tareas', compact('tareas'));
+}
+
+
+    public function update(Request $request, $id)
+    {
+        $Tareas = Post::findOrFail($id);
+
+        $validation = Validator::make($request->all(), [
+            'titulo' => 'nullable|string|max:255',
+            'contenido' => 'nullable|string|max:255',
+            'estado' => 'nullable|string|max:255',
+            'autor' => 'required|string|max:500',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json($validation->errors(), 400);
+        }
+
+        $Tareas->titulo = $request->input('titulo');
+        $Tareas->contenido = $request->input('contenido');
+        $Tareas->estado = $request->input('estado');
+        $Tareas->autor = $request->input('autor');
+        $Tareas->save();
+
+        return response()->json(['message' => 'Se creo la tarea '], 200);
+    }
+
+    public function destroy($id)
+    {
+        $Tareas = Post::findOrFail($id);
+
+
+        $post->delete();
+
+        return response()->json(['message' => 'Se elimino el deber'], 200);
+    }
+
+    public function create(Request $request){
+        $validation = Validator::make($request->all(), [
+            'titulo' => 'nullable|string|max:255',
+            'contenido' => 'nullable|string|max:255',
+            'estado' => 'nullable|string|max:255',
+            'autor' => 'required|string|max:500',
+        ]);
+
+        if ($validation->fails()) {
+            return $validation->errors();
+        }
+
+        return $this->createUser($request);
+    }
+
+
+    public function buscar(Request $request)
+    {
+        $titulo = $request->input('titulo');
+        $autor = $request->input('autor');
+        $estado = $request->input('estado');
+
+        $query = Tarea::query();
+
+        if ($titulo) {
+            $query->where('titulo', 'like', "%$titulo%");
+        }
+
+        if ($autor) {
+            $query->where('autor', 'like', "%$autor%");
+        }
+
+        if ($estado) {
+            $query->where('estado', $estado);
+        }
+
+        return $query->get();
+        return view('tareas', compact('tareas'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function ()
+{
+    Route::get('/tareas', [Tareas::class, 'index'])->name('tareas.index');
+    Route::get('tareas/buscar', 'Tareas@buscar');
+    Route::resource('tareas', 'Tareas');
+    Route::post('/tareas',[UserController::class,"update"]);
+    Route::post('/tareas',[UserController::class,"destroy"]);
+    Route::post('/tareas',[UserController::class,"update"]);
 });
